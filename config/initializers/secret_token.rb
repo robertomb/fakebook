d17041d0d92ac78fbf5e0300fc17dad86b4f1f04
Roberto Braga <bragarmb@gmail.com>
2012-12-03 21:37:05 -0200

    First Commit - Added Slim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakebook::Application.config.secret_token = 'a4d54de6ab7d7a4759132098adf26927fe718166ae46b27d57025c058c1ad9e147a2568699a60889a47d1b8accbcbe763414f70b27bfe4aebdf67fd1e36733db'
